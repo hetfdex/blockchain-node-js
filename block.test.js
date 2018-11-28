b9e4f2784fe0d86de5eb7c8e330008d99cd37488
Jose Pereira <hetfdex@gmail.com>
2018-11-28 16:30:46 +0000

    Block basis implemented & tests

--- block.test.js
+++ block.test.js
@@ -1,3 +1,6 @@
+const Block = require("./block");
+const {GENESIS_DATA} = require("./config");
+
 describe("Block", () => {
   const timestamp = "timestamp";
   const lastHash = "lastHash";
@@ -6,10 +9,31 @@ describe("Block", () => {
 
   const block = new Block({timestamp, lastHash, hash, data});
 
-  it("has all four base properties", () => {
+  it("has timestamp", () => {
     expect(block.timestamp).toEqual(timestamp);
+  });
+
+  it("has lastHash", () => {
     expect(block.lastHash).toEqual(lastHash);
+  });
+
+  it("has hash", () => {
     expect(block.hash).toEqual(hash);
+  });
+
+  it("has data", () => {
     expect(block.data).toEqual(data);
-  })
+  });
+
+  describe("genesis()", () => {
+    const genesisBlock = Block.genesis();
+
+    it("returns block instance", () => {
+      expect(genesisBlock instanceof Block).toBe(true);
+    });
+
+    it("returns genesis data", () => {
+      expect(genesisBlock).toEqual(GENESIS_DATA);
+    });
+  });
 });
